59d37afedd9d4f041c089563a762e12743b52b0d
grejaili <gabrielrejaili@gmail.com>
2017-06-03 16:50:02 -0400

    Attacking ON

--- Source/TrashPanda/AI/BTService_SelectTarget.cpp
+++ Source/TrashPanda/AI/BTService_SelectTarget.cpp
@@ -1,15 +1,10 @@
 // All Rights Reserved for Students Graduating TFS Summer 2017
 
 #include "TrashPanda.h"
-<<<<<<< HEAD
-=======
-#include "BTService_SelectTarget.h"
->>>>>>> refs/remotes/origin/Raph_Branch
 #include "BehaviorTree/BehaviorTree.h"
 #include "BehaviorTree/BehaviorTreeComponent.h"
 #include "BehaviorTree/BlackboardComponent.h"
 #include "BehaviorTree/Blackboard/BlackboardKeyAllTypes.h"
-<<<<<<< HEAD
 #include "Perception/AIPerceptionComponent.h"
 #include "Perception/AISense_Sight.h"
 #include "Player/Chip.h"
@@ -21,15 +16,6 @@
 
 #define print(text) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Red,text) 
 
-=======
-#include "Player/Chip.h"
-#include "AIController.h"
-#include "Perception/AIPerceptionComponent.h"
-#include "Perception/AISense_Sight.h"
-#include "AI/Enemy.h"
-#include "AI/EnemyAI.h"
-#include "AI/AI_TargetLocation.h"
->>>>>>> refs/remotes/origin/Raph_Branch
 
 void UBTService_SelectTarget::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
 {
@@ -39,85 +25,22 @@ void UBTService_SelectTarget::TickNode(UBehaviorTreeComponent& OwnerComp, uint8*
 	UAIPerceptionComponent* Perception = OwnerComp.GetAIOwner()->GetPerceptionComponent();
 
 	AActor* FoundTarget = FindTarget(Perception, OwningCharacter);
-<<<<<<< HEAD
 
 	OwnerComp.GetBlackboardComponent()->SetValue<UBlackboardKeyType_Object>(TEXT("Target"), FoundTarget);
-<<<<<<< HEAD
-=======
-	OwnerComp.GetBlackboardComponent()->SetValue<UBlackboardKeyType_Object>(TEXT("Target"), FoundTarget);
-
-	ExecuteTask(OwnerComp, NodeMemory);
-
-}
-
-EBTNodeResult::Type UBTService_SelectTarget::ExecuteTask(UBehaviorTreeComponent & OwnerComp, uint8 * NodeMemory)
-{
-	AEnemyAI* AICon = Cast<AEnemyAI>(OwnerComp.GetAIOwner());
-
-	if (AICon)
-	{
-		//Get BB component
-		UBlackboardComponent * BlackboardComp = AICon->GetBlackboardComp();
-
-		AAI_TargetLocation * CurrentPoint = Cast<AAI_TargetLocation>(BlackboardComp->GetValueAsObject("PatrolPoint"));
-
-		TArray<AActor*> AvailablePatrolPoints = AICon->GetPatrolPoints();
-
-		int32 RandomIndex;
-
-		AAI_TargetLocation* NextPatrolPoint = nullptr;
-
-		do 
-		{
-			RandomIndex = FMath::RandRange(0, AvailablePatrolPoints.Num() - 1);
-						
-			UE_LOG(LogTemp, Warning, TEXT("Available Patrol Points: %d"), AvailablePatrolPoints.Num());
-
-			NextPatrolPoint = Cast<AAI_TargetLocation>(AvailablePatrolPoints[RandomIndex]);
-
-		} while (CurrentPoint == NextPatrolPoint);
-
-		BlackboardComp->SetValueAsObject("PatrolPoint", NextPatrolPoint);
-
-		return EBTNodeResult::Succeeded;
-	}
-
-	return EBTNodeResult::Failed;
->>>>>>> refs/remotes/origin/Raph_Branch
-=======
 	if (FoundTarget != NULL)
 	{
 		OwnerComp.GetBlackboardComponent()->SetValue<UBlackboardKeyType_Bool>(TEXT("InCombat"), true);
-	
 	}
->>>>>>> origin/Gabe_Branch
 }
 
 AActor* UBTService_SelectTarget::FindTarget(class UAIPerceptionComponent* PerceptionComponent, class AChip* OwningCharacter)
 {
 	TArray<AActor*> Hostiles;
 	PerceptionComponent->GetHostileActors(Hostiles);
-<<<<<<< HEAD
 	if (Hostiles.Num() > 0)
 	{
 
-		return Hostiles[0];	
-	}
-	return NULL;
-}
-=======
-
-	if (Hostiles.Num() > 0)
-	{
 		return Hostiles[0];
 	}
-
 	return NULL;
 }
-
-
-
-
-
-
->>>>>>> refs/remotes/origin/Raph_Branch
--- Source/TrashPanda/AI/BTService_SelectTarget.h
+++ Source/TrashPanda/AI/BTService_SelectTarget.h
@@ -3,29 +3,21 @@
 #pragma once
 
 #include "BehaviorTree/Services/BTService_BlackboardBase.h"
-<<<<<<< HEAD
-=======
-#include "BehaviorTree/BTTaskNode.h"
->>>>>>> refs/remotes/origin/Raph_Branch
 #include "BTService_SelectTarget.generated.h"
 
 /**
- * 
- */
+*
+*/
 UCLASS()
 class TRASHPANDA_API UBTService_SelectTarget : public UBTService_BlackboardBase
 {
 	GENERATED_BODY()
-	
+
 public:
 	void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
 
 protected:
 	virtual AActor* FindTarget(class UAIPerceptionComponent* PerceptionComponent, class AChip* OwningCharacter);
-	
-<<<<<<< HEAD
-=======
-	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory);
->>>>>>> refs/remotes/origin/Raph_Branch
-	
+
+
 };
